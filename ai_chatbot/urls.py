d7d20e0bffefee01149185becf9680b6043ce068
sajidsh65 <158176359+sajidsh65@users.noreply.github.com>
2025-05-09 18:21:27 +0500

    Update urls.py

--- ai_chatbot/urls.py
+++ ai_chatbot/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("🎉 Backend is live and working!")),
     path('admin/', admin.site.urls),
     path('api/', include('myPred.urls')),
 ]
